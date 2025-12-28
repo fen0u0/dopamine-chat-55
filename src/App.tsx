855da54995a505bc07b32688ba0121122d2066e2
Prithwis Dutta <prithwis1201@gmail.com>
2025-12-28 20:21:11 +0530

    Remove BrowserRouter and restructure Routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { GemsProvider } from "@/contexts/GemsContext";
 import { ChatProvider } from "@/contexts/ChatContext";
 import { SettingsProvider } from "@/contexts/SettingsContext";
+
 import Index from "./pages/Index";
 import Chats from "./pages/Chats";
 import Chat from "./pages/Chat";
@@ -24,17 +25,17 @@ const App = () => (
           <TooltipProvider>
             <Toaster />
             <Sonner />
-            <BrowserRouter>
-              <Routes>
-                <Route path="/" element={<Index />} />
-                <Route path="/chats" element={<Chats />} />
-                <Route path="/chat/:id" element={<Chat />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/settings" element={<Settings />} />
-                <Route path="/confessions" element={<Confessions />} />
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-            </BrowserRouter>
+
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="/chats" element={<Chats />} />
+              <Route path="/chat/:id" element={<Chat />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/settings" element={<Settings />} />
+              <Route path="/confessions" element={<Confessions />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+
           </TooltipProvider>
         </ChatProvider>
       </SettingsProvider>
